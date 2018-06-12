cc7fe31c5b85fdc3405cf5d7e25bc1e00a422475
David Vaquero <pepesan@gmail.com>
2018-06-12 15:56:54 +0200

    Uso Básico de las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
